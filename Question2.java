755fe5c3831b49f757ae6ecf2adb03e35c113778
Prajwal-0510 <155533104+Prajwal-0510@users.noreply.github.com>
2026-03-14 12:35:40 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,17 @@
+public class Question2 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int s = i; s < n; s++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
