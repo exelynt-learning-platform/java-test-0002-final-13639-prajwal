a70708334b686730b5dd68ce2419dbeb2984a5f3
Prajwal-0510 <155533104+Prajwal-0510@users.noreply.github.com>
2026-03-14 16:48:01 +0530

    Refactor to create MirroredNumberPyramid class
    Updated Code

--- Question2.java
+++ Question2.java
@@ -1,9 +1,9 @@
-public class Question2 {
+public class MirroredNumberPyramid {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
-            for (int s = i; s < n; s++) {
-                System.out.print("  ");
+            for (int j = 1; j <= (n - i) * 2; j++) {
+                System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
